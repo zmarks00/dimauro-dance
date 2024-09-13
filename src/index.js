1d68f9eec8a0a1aac8c1c6ce4ade1e65e8cd07b1
Scott <scottcanoni@gmail.com>
2024-09-12 20:20:49 -0400

    Cleanup, added color palette

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import './custom.scss';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
