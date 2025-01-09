45b1aa07f77efe4a82dc84e91443d5469e30a130
zmarks00 <122951066+zmarks00@users.noreply.github.com>
2025-01-09 11:22:12 -0500

    removed unnecessary content and added some functionality

--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
-import './App.scss';
-import Homepage from './Homepage';
-import Nav from './Nav';
+import "./App.scss";
+import Homepage from "./Homepage";
+import Nav from "./Nav";
 
 function App() {
   return (
-      <div className="App">
-          <Nav />
+    <div className="App">
+      <Nav />
 
-          <Homepage />
+      <Homepage />
 
-          <footer id="footer" className="footer position-relative dark-background">
-              <div className="container">
-                  <h3 className="sitename">Jessie<br/></h3>
-                  <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
-                  <div className="social-links d-flex justify-content-center">
-                      {/* <a href=""><i className="bi bi-twitter-x"></i></a> */}
-                      <a href=""><i className="bi bi-facebook"></i></a>
-                      <a href=""><i className="bi bi-instagram"></i></a>
-                      {/* <a href=""><i className="bi bi-skype"></i></a> */}
-                      {/* <a href=""><i className="bi bi-linkedin"></i></a> */}
-                  </div>
-                  <div className="container">
-                      <div className="copyright">
-                          <span>Copyright</span> <strong className="px-1 sitename">Jessica DiMauro Marks</strong> <span>All Rights Reserved</span>
-                      </div>
-                      <div className="credits">
+      <footer id="footer" className="footer position-relative dark-background">
+        <div className="container">
+          {/* <h3 className="sitename">
+            Jessica DiMauro Marks
+            <br />
+          </h3> */}
+          <p></p>
+          <div className="social-links d-flex justify-content-center">
+            {/* <a href=""><i className="bi bi-twitter-x"></i></a> */}
+            <a
+              href="https://www.facebook.com/dimaurodancecompany/"
+              target="_blank"
+            >
+              <i className="bi bi-facebook"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/dimaurodance/?hl=en"
+              target="_blank"
+            >
+              <i className="bi bi-instagram"></i>
+            </a>
+            {/* <a href=""><i className="bi bi-skype"></i></a> */}
+            {/* <a href=""><i className="bi bi-linkedin"></i></a> */}
+          </div>
+          <div className="container">
+            <div className="copyright">
+              <span>Copyright</span>{" "}
+              <strong className="px-1 sitename">Jessica DiMauro Marks</strong>{" "}
+              <span>All Rights Reserved</span>
+            </div>
+            <div className="credits">
+              This program has been made possible in part through the
+              sponsorship of{" "}
+              <a
+                className="donate__button-link"
+                href="https://www.thefield.org/"
+                target="_blank"
+              >
+                {" "}
+                The Field
+              </a>
+              .
+            </div>
+          </div>
+        </div>
+      </footer>
 
-                      This program has been made possible in part through the sponsorship of <a href="https://www.thefield.org/" target="_blank"> The Field</a>.
-                      </div>
-                  </div>
-              </div>
-          </footer>
+      <a
+        href="#"
+        id="scroll-top"
+        className="scroll-top d-flex align-items-center justify-content-center"
+      >
+        <i className="bi bi-arrow-up-short"></i>
+      </a>
 
-          <a href="#" id="scroll-top" className="scroll-top d-flex align-items-center justify-content-center"><i className="bi bi-arrow-up-short"></i></a>
-          
-          <div id="preloader"></div>
-      </div>
+      <div id="preloader"></div>
+    </div>
   );
 }
 
